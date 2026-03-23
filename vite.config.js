5c53a01d12c16395374cea85592718c011e5e77d
louishermanpaelinck <louisherman.paelinck@student.sintjorisbazel.be>
2026-03-23 14:03:22 +0000

    host

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ const config = ({ mode }) => {
         registerType: "autoUpdate",
       }),
     ],
-    base: "",
+    base: "/w11-react/",
     define: {
       "process.env.NODE_ENV": `"${mode}"`,
     },
